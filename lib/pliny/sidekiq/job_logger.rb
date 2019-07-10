5188f3b9cc22d3c80d7b01a1195a1e9ca3c52ffe
Ben Fritsch <beanie@benle.de>
2019-07-10 11:47:33 +0200

    add JobLogger, this will allow us to configure Sidekiq to use this job logger. This simplifies the tracking of failed/successful job the first line for each job will look like this:
    ```
    job_jid=03032bbe4202a223f9933581 job_retry=0 job_logger at=start
    ```
    
    instead of
    ```
    JID-03032bbe4202a223f9933581 INFO: start
    ```
    
    the last line for each job will look like
    ```
    job_id=03032bbe4202a223f9933581 job_retry=0 job_logger at=finish status=done duration=0.006
    ```
    
    instead of 
    
    ```
    JID-03032bbe4202a223f9933581 INFO: done: 0.007 sec
    
    ```

---
+++ lib/pliny/sidekiq/job_logger.rb
@@ -0,0 +1,32 @@
+module Pliny::Sidekiq
+  class JobLogger
+    def call(job, queue)
+      context = {
+        sidekiq: true,
+        job:     job['class'],
+        job_jid:  job['jid'],
+        job_retry: job['retry'],
+      }
+
+      begin
+        start = ::Process.clock_gettime(::Process::CLOCK_MONOTONIC)
+        log(context, job_logger: true, at: :start)
+        yield
+        log(context, job_logger: true, at: :finish, status: :done, duration: elapsed(start))
+      rescue Exception
+        log(context, job_logger: true, at: :finish, status: :fail, duration: elapsed(start))
+        raise
+      end
+    end
+
+    private
+
+    def log(context, data = {}, &blk)
+      Pliny.log(context.merge(data), &blk)
+    end
+
+    def elapsed(start)
+      (::Process.clock_gettime(::Process::CLOCK_MONOTONIC) - start).round(3)
+    end
+  end
+end
